b5a4449e2510edefca5174701340fe3d0e4771ec
JTdeska <143404155+JTdeska@users.noreply.github.com>
2024-01-01 08:17:12 +0000

    main

--- pointer.cpp
+++ pointer.cpp
@@ -1,58 +1,15 @@
-#include <stdio.h>
-#include <vector>
+#include <iostream>
+#include <string>
 using namespace std;
-
-vector<int> buf;
-
-void gcd(int invalue)
-{
-	for (int i = 2; i <= invalue; i++)
-	{
-		if ((invalue%i) == 0)
-		{
-			buf.push_back(i);
-			gcd(invalue / i);
-			return;
-		}
-	}
+void p(int *ans){
+	cout<<ans<<endl;
+	return;
 }
-
-int main()
-{
-	int invalue;
-
-	//printf("Input Value: ");
-	scanf("%d", &invalue);
-	//getchar();
-
-	gcd(invalue);
-
-	for (int i = 0; i < buf.size(); i++)
-	{
-		int count = 1;
-
-		for (int j = i + 1; j < buf.size(); j++)
-		{
-			if (buf[j] == buf[i])
-				count++;
-			else
-				break;
-		}
-
-		if (i > 0)
-			printf("* ");
-
-		if (count > 1)
-		{
-			printf("%d^%d ", buf[i], count);
-			i += (count - 1);
-		}
-		else
-			printf("%d ", buf[i]);
-	}
-
-	//printf("\nPress any key to Exit.");
-	//getchar();
+int main(){
+	string str;
+	int ans1;
+	cin>>str>>ans1;
+	p(ans1)	;
     return 0;
 }
 
