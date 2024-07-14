e878ae04b1dd81d0a04d2e3449022f4051e43491
JTdeska <143404155+JTdeska@users.noreply.github.com>
2024-07-14 06:00:46 +0000

    main

--- test.cpp
+++ test.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-    
+    int n;
+    int sum;
+    while(cin>>n){
+        int a;
+        sum=0;
+        for(int i=0;i<n;i++){
+            cin>>a;
+            sum+=a;
+        }
+        if(sum/n>59)
+            cout<<"no"<<endl;
+        else
+            cout<<"yes"<<endl;
+    }
     return 0;
 }
